a9078c7d7e91151874aa27d408fc9fbe68dcca7a
Rich Greenhill <rgnet1@yahoo.com>
2016-12-05 09:54:16 -0800

    first deploy to heroku

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+console.log(path.join(__dirname, '../app.js'));
+
+// app.use(express.static(path.join(__dirname, '')));
