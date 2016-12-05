0a6b937478fd9c6df72010b0930ce10423995cc2
Rich Greenhill <rgnet1@yahoo.com>
2016-12-05 10:35:22 -0800

    add API server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+
 const app = express();
 
-console.log(path.join(__dirname, '../app.js'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
+}
 
-// app.use(express.static(path.join(__dirname, '')));
+const allowCors = function(req, res, next){
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+};
+
+app.use('/api/v1/*', allowCors);
+
+app.get('/api/v1/messages', (req, res) => {
+  res.json({ message: 'howdy' });
+});
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+fetch('http://localhost:8080/api/v1/messages')
+  .then(res => res.json())
+  .then(console.log)
+  .catch(console.error);
