6dff7af051fb0f36bcbcc441dc77f225eeb75189
Rich Greenhill <rgnet1@yahoo.com>
2016-12-05 19:49:26 -0800

    add SERVER_URL config

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const HOSTNAME = process.env.NODE_ENV === 'production' ?
-  '' :
-  'http://localhost:8080';
+import { SERVER_URL } from './config';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
 
-fetch(`${HOSTNAME}/api/v1/messages`)
+fetch(`${SERVER_URL}/api/v1/messages`)
   .then(res => res.json())
   .then(console.log)
   .catch(console.error);
